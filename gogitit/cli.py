e87c0d1499b9ee47a11330a6b360494425a44cd6
Devan Goodwin <dgoodwin@redhat.com>
2017-08-13 08:29:34 -0300

    Implement initial file copying.

--- gogitit/cli.py
+++ gogitit/cli.py
@@ -1,11 +1,6 @@
 import click
 import gogitit.manifest
-import gogitit.clone
 import os
-
-@click.group()
-def main():
-    pass
 
 @click.command()
 @click.option('--manifest', '-m', default='manifest.yml', type=click.File('r'),
@@ -13,28 +8,43 @@ def main():
 @click.option('--cache-dir', default=os.path.expanduser('~/.gogitit/cache'),
         type=click.Path(file_okay=False, dir_okay=True, writable=True, readable=True),
         help="Directory where gogitit will store cached copies of repositories.")
-def sync(manifest, cache_dir):
+@click.option('--output-dir', '-o', default=os.path.abspath('./'),
+        type=click.Path(file_okay=False, dir_okay=True, writable=True, readable=True),
+        help="Directory where all output will be assembled into final structure.")
+def sync(manifest, cache_dir, output_dir):
     """Fetch all remote sources and assemble into the destination directory."""
-    click.echo("Executing sync")
-    m = gogitit.manifest.load(manifest)
-    click.echo(m.repos[0])
-    for t in m.repos[0].transfers:
-        click.echo("- %s" % t)
-
+    click.echo("Executing sync to output directory: %s" % output_dir)
     # Make sure the working directory exists:
     if not os.path.exists(cache_dir):
         click.echo("Creating gogitit cache directory: %s" % cache_dir)
         os.makedirs(cache_dir)
 
+    manifest = gogitit.manifest.load(manifest, cache_dir)
+    click.echo(manifest.repos[0])
+    for copy in manifest.repos[0].copy:
+        click.echo("- %s" % copy)
+
     # Clone/update all repos:
-    for r in m.repos:
-        cloner = gogitit.clone.GitCloner(cache_dir, r)
-        cloner.clone()
+    for repo in manifest.repos:
+        repo.clone()
+        for copy in repo.copy:
+            copy.validate()
+
+    for repo in manifest.repos:
+        for copy in repo.copy:
+            copy.run(output_dir)
 
 @click.command()
 def check():
-    """Scan the destination directory and it's cache and check if contents match current manifest."""
+    """
+    Scan the destination directory and it's cache and check if contents
+    match current manifest.
+    """
     click.echo("Checking destination directory.")
+
+@click.group()
+def main():
+    pass
 
 main.add_command(sync)
 main.add_command(check)
--- gogitit/manifest.py
+++ gogitit/manifest.py
@@ -1,37 +1,110 @@
 """Parses the gogitit manifest."""
 
+import os
+import glob
+import shutil
+
+import click
+import git
 import yaml
 
-def load(f):
+def load(f, cache_dir):
     data = yaml.safe_load(f)
     # TODO: validation
-    m = Manifest(**data)
+    m = Manifest(cache_dir, **data)
     return m
 
+
 class Manifest(object):
-    def __init__(self, **kwargs):
+    def __init__(self, cache_dir, **kwargs):
+        self.cache_dir = cache_dir
         self.repos = []
         for r in kwargs['repos']:
-            self.repos.append(Repo(**r))
+            self.repos.append(Repo(cache_dir, **r))
+
 
 class Repo(object):
-    def __init__(self, **kwargs):
+    def __init__(self, cache_dir, **kwargs):
+        self.cache_dir = cache_dir
+
         # TODO: repoid must be valid for a directory name as we use it in caches:
         self.id = kwargs['id']
+
         self.url = kwargs['url']
         self.version = kwargs.get('version', 'master')
-        self.transfers = []
-        for t in kwargs['transfers']:
-            self.transfers.append(Transfer(**t))
+
+        # Location where we maintain a git clone of this repo:
+        self.repo_dir = os.path.join(self.cache_dir, self.id)
+
+        self.copy = []
+        for t in kwargs['copy']:
+            self.copy.append(Copy(self.repo_dir, **t))
 
     def __str__(self):
         return "Repo<id=%s url=%s version=%s>" % (self.id, self.url, self.version)
 
-class Transfer(object):
-    def __init__(self, **kwargs):
+    def clone(self):
+        if not os.path.exists(self.repo_dir):
+            click.echo("Creating repo cache: %s" % self.repo_dir)
+            os.makedirs(self.repo_dir)
+            click.echo("Cloning %s" % self.url)
+            git_repo = git.Repo.clone_from(self.url, self.repo_dir)
+        else:
+            click.echo("Re-using repo cache: %s" % self.repo_dir)
+
+        git_repo = git.Repo(self.repo_dir)
+        git_repo = git.Git(self.repo_dir)
+        click.echo("Checking out version: %s" % self.version)
+        git_repo.fetch('origin')
+        git_repo.checkout(self.version)
+
+        # Try to git pull, but this won't work if version is a tag or commit:
+        try:
+            git_repo.pull()
+        except git.exc.GitCommandError as e:
+            if 'You are not currently on a branch' in e.stderr:
+                pass
+            else:
+                raise e
+
+
+class Copy(object):
+    def __init__(self, repo_dir, **kwargs):
+        self.repo_dir = repo_dir
         self.src = kwargs['src']
         self.dst = kwargs['dst']
 
+        # Will contain globbed files and directories we intend to copy over.
+        self.files_matched = []
+
     def __str__(self):
-        return "Transfer<src=%s dst=%s>" % (self.src, self.dst)
+        return "Copy<src=%s dst=%s>" % (self.src, self.dst)
+
+    def validate(self):
+        source = os.path.join(self.repo_dir, self.src)
+        click.echo("source = %s" % source)
+        mode = None
+        self.files_matched = glob.glob(source)
+        click.echo(self.files_matched)
+        if len(self.files_matched) == 0 and not os.path.exists(source):
+            raise click.ClickException("src does not exist in repo: %s" % source)
+
+    def run(self, output_dir):
+        # List of tuples, source file or path, dest path:
+        fulldest = os.path.join(output_dir, self.dst)
+        copy_pairs = []
+        for match in self.files_matched:
+            copy_pairs.append((match, fulldest))
+
+        for pair in copy_pairs:
+            click.echo("%s -> %s" % pair)
+            if os.path.isdir(pair[0]):
+                shutil.copytree(pair[0], pair[1])
+            else:
+                # Make sure directory exists for the file copy:
+                dest_dir = os.path.dirname(os.path.abspath(pair[1]))
+                if not os.path.exists(dest_dir):
+                    os.makedirs(dest_dir)
+                shutil.copy2(pair[0], pair[1])
+
 
